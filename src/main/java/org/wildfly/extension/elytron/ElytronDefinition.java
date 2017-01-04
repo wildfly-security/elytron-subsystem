2c8cdd537c2daedccf9f6aba8d66cd1134a0a986
Darran Lofthouse <darran.lofthouse@jboss.com>
2017-01-04 14:28:19 +0000

    Resolves #342 Add DeploymentUnitProcessors to associate an AuthenticationContext with the ClassLoader of deployments.

--- src/main/java/org/wildfly/extension/elytron/ElytronDefinition.java
+++ src/main/java/org/wildfly/extension/elytron/ElytronDefinition.java
@@ -18,6 +18,8 @@
 
 package org.wildfly.extension.elytron;
 
+import static org.wildfly.extension.elytron.Capabilities.AUTHENTICATION_CONTEXT_CAPABILITY;
+import static org.wildfly.extension.elytron.Capabilities.ELYTRON_RUNTIME_CAPABILITY;
 import static org.wildfly.extension.elytron.Capabilities.MODIFIABLE_SECURITY_REALM_RUNTIME_CAPABILITY;
 import static org.wildfly.extension.elytron.Capabilities.PERMISSION_MAPPER_RUNTIME_CAPABILITY;
 import static org.wildfly.extension.elytron.Capabilities.PRINCIPAL_DECODER_RUNTIME_CAPABILITY;
@@ -31,14 +33,22 @@ import static org.wildfly.extension.elytron._private.ElytronSubsystemMessages.RO
 
 import org.jboss.as.controller.AbstractBoottimeAddStepHandler;
 import org.jboss.as.controller.AbstractRemoveStepHandler;
+import org.jboss.as.controller.AbstractWriteAttributeHandler;
 import org.jboss.as.controller.OperationContext;
 import org.jboss.as.controller.OperationContext.AttachmentKey;
+import org.jboss.as.controller.OperationContext.Stage;
 import org.jboss.as.controller.OperationFailedException;
 import org.jboss.as.controller.ResourceDefinition;
+import org.jboss.as.controller.SimpleAttributeDefinition;
+import org.jboss.as.controller.SimpleAttributeDefinitionBuilder;
 import org.jboss.as.controller.SimpleResourceDefinition;
 import org.jboss.as.controller.registry.ManagementResourceRegistration;
 import org.jboss.as.controller.registry.Resource;
+import org.jboss.as.server.AbstractDeploymentChainStep;
+import org.jboss.as.server.DeploymentProcessorTarget;
+import org.jboss.as.server.deployment.Phase;
 import org.jboss.dmr.ModelNode;
+import org.jboss.dmr.ModelType;
 import org.jboss.msc.service.Service;
 import org.jboss.msc.service.ServiceBuilder;
 import org.jboss.msc.service.ServiceController;
@@ -64,14 +74,21 @@ import org.wildfly.security.authz.RoleMapper;
  */
 class ElytronDefinition extends SimpleResourceDefinition {
 
-    public static final ElytronDefinition INSTANCE = new ElytronDefinition();
-
     private static final AttachmentKey<SecurityPropertyService> SECURITY_PROPERTY_SERVICE_KEY = AttachmentKey.create(SecurityPropertyService.class);
 
+    private static final AuthenticationContextDependencyProcessor AUTHENITCATION_CONTEXT_PROCESSOR = new AuthenticationContextDependencyProcessor();
+
+    static final SimpleAttributeDefinition DEFAULT_AUTHENTICATION_CONTEXT = new SimpleAttributeDefinitionBuilder(ElytronDescriptionConstants.DEFAULT_AUTHENTICATION_CONTEXT, ModelType.STRING, true)
+            .setCapabilityReference(AUTHENTICATION_CONTEXT_CAPABILITY, ELYTRON_RUNTIME_CAPABILITY)
+            .build();
+
+    public static final ElytronDefinition INSTANCE = new ElytronDefinition();
+
     private ElytronDefinition() {
-        super(ElytronExtension.SUBSYSTEM_PATH,
-                ElytronExtension.getResourceDescriptionResolver(),
-                new ElytronAdd(),new ElytronRemove());
+        super(new Parameters(ElytronExtension.SUBSYSTEM_PATH, ElytronExtension.getResourceDescriptionResolver())
+                .setAddHandler(new ElytronAdd())
+                .setRemoveHandler(new ElytronRemove())
+                .setCapabilities(ELYTRON_RUNTIME_CAPABILITY));
     }
 
     @Override
@@ -180,6 +197,30 @@ class ElytronDefinition extends SimpleResourceDefinition {
         resourceRegistration.registerSubModel(AuthenticationClientDefinitions.getAuthenticationContextDefinition());
     }
 
+    @Override
+    public void registerAttributes(ManagementResourceRegistration resourceRegistration) {
+        resourceRegistration.registerReadWriteAttribute(DEFAULT_AUTHENTICATION_CONTEXT, null, new AbstractWriteAttributeHandler<Void>(DEFAULT_AUTHENTICATION_CONTEXT) {
+
+            @Override
+            protected boolean applyUpdateToRuntime(OperationContext context, ModelNode operation, String attributeName,
+                    ModelNode resolvedValue, ModelNode currentValue,
+                    HandbackHolder<Void> handbackHolder)
+                    throws OperationFailedException {
+                AUTHENITCATION_CONTEXT_PROCESSOR.setDefaultAuthenticationContext(resolvedValue.isDefined() ? resolvedValue.asString() : null);
+                return false;
+            }
+
+            @Override
+            protected void revertUpdateToRuntime(OperationContext context, ModelNode operation, String attributeName,
+                    ModelNode valueToRestore, ModelNode valueToRevert, Void handback)
+                    throws OperationFailedException {
+                AUTHENITCATION_CONTEXT_PROCESSOR.setDefaultAuthenticationContext(valueToRestore.isDefined() ? valueToRestore.asString() : null);
+            }
+
+        });
+
+    }
+
     static <T> ServiceBuilder<T>  commonDependencies(ServiceBuilder<T> serviceBuilder) {
         serviceBuilder.addDependencies(SecurityPropertyService.SERVICE_NAME);
         serviceBuilder.addDependencies(CoreService.SERVICE_NAME);
@@ -210,25 +251,39 @@ class ElytronDefinition extends SimpleResourceDefinition {
     private static class ElytronAdd extends AbstractBoottimeAddStepHandler {
 
         private ElytronAdd() {
+            super(ELYTRON_RUNTIME_CAPABILITY, DEFAULT_AUTHENTICATION_CONTEXT);
         }
 
         @Override
         protected void populateModel(ModelNode operation, ModelNode model) throws OperationFailedException {
             ROOT_LOGGER.activatingElytronSubsystem(org.wildfly.security.Version.getVersion(), Version.getVersion());
+            super.populateModel(operation, model);
         }
 
         @Override
-        protected void performBoottime(OperationContext context, ModelNode operation, Resource resource)
-                throws OperationFailedException {
+        protected void performBoottime(OperationContext context, ModelNode operation, Resource resource) throws OperationFailedException {
+            ModelNode model = resource.getModel();
+            ModelNode defaultAuthenticationContext = DEFAULT_AUTHENTICATION_CONTEXT.resolveModelAttribute(context, model);
+            AUTHENITCATION_CONTEXT_PROCESSOR.setDefaultAuthenticationContext(defaultAuthenticationContext.isDefined() ? defaultAuthenticationContext.asString() : null);
+
             ServiceTarget target = context.getServiceTarget();
             installService(SecurityPropertyService.SERVICE_NAME, new SecurityPropertyService(), target);
             installService(CoreService.SERVICE_NAME, new CoreService(), target);
+
+            context.addStep(new AbstractDeploymentChainStep() {
+                @Override
+                protected void execute(DeploymentProcessorTarget processorTarget) {
+                    processorTarget.addDeploymentProcessor(ElytronExtension.SUBSYSTEM_NAME, Phase.CONFIGURE_MODULE, Phase.CONFIGURE_AUTHENTICATION_CONTEXT, AUTHENITCATION_CONTEXT_PROCESSOR);
+                    processorTarget.addDeploymentProcessor(ElytronExtension.SUBSYSTEM_NAME, Phase.FIRST_MODULE_USE, Phase.FIRST_MODULE_USE_AUTHENTICATION_CONTEXT, new AuthenticationContextAssociationProcessor());
+                }
+            }, Stage.RUNTIME);
         }
 
         @Override
         protected void rollbackRuntime(OperationContext context, ModelNode operation, Resource resource) {
             uninstallSecurityPropertyService(context);
             context.removeService(CoreService.SERVICE_NAME);
+            AUTHENITCATION_CONTEXT_PROCESSOR.setDefaultAuthenticationContext(null);
         }
 
     }
@@ -236,6 +291,7 @@ class ElytronDefinition extends SimpleResourceDefinition {
     private static class ElytronRemove extends AbstractRemoveStepHandler {
 
         private ElytronRemove() {
+            super(ELYTRON_RUNTIME_CAPABILITY);
         }
 
         @Override
